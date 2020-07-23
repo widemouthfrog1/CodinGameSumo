e131bf76891149c1651f00549d863103ebcd6f3f
Karl Bennett <widemouthfrog1@gmail.com>
2020-07-23 16:38:12 +1200

    Initial Commit

--- src/main/resources/view/config.js
+++ src/main/resources/view/config.js
@@ -1,5 +1,7 @@
 import { GraphicEntityModule } from './entity-module/GraphicEntityModule.js';
 
+export const gameName = 'Sumo';
+
 // List of viewer modules that you want to use in your game
 export const modules = [
 	GraphicEntityModule
